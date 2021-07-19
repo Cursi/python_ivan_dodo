7ef02e6fff2a754f5ea0c8852696d8e11bdaf062
DoinaMT <tomescudoina98@gmail.com>
2021-07-19 17:26:44 +0300

    Updated

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-import numpy as np
+from functools import reduce
 
-print("Mi-e foame") 
+x = [1, 2, 3, 4, 5, 6]
 
-print (np.linspace(1,10))
+# y = []
+
+# def myfilterFunction(currentElement):
+#     return currentElement % 2 == 1
+
+# for c in x:
+#     y.append(c ** 2)
+
+# z=[c ** 2 for c in x if c % 2 == 0]
+
+# W=list(filter(myfilterFunction, x))
+# T=list(filter(lambda x: x % 2, x))
+# print(W)
+
+print(x)
+# print(y)
+# print(z)
+# print(T)
+
+# u = list(map(lambda x: x ** 3, x))
+# print(u)
+
+R = reduce(lambda m,n: m+n, x)
+
+print(R)
