90284b5b5e26f6ac85d062dfa6f0871fe5db2290
Ivan <Aivan@luxoft.com>
2021-07-19 17:29:08 +0300

    Updated fresh programs

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-import numpy as np
+from functools import reduce
 
-print(np.linspace(1, 10))
+X = [1, 2, 3, 4, 5, 6]
+
+# def myFilterFunction(currentElement):
+#     return currentElement % 2
+
+# Y = []
+
+# for c in X:
+#     Y.append(c ** 2)
+
+# Z = [c ** 2 for c in X if c % 2 == 0]
+
+# W = list(filter(myFilterFunction, X))
+
+# T = list(filter(lambda x: x % 2, X))
+
+print(X)
+# print(Y)
+# print(W)
+# print(T)
+
+# u = list(map(lambda x: x ** 3, X))
+
+# print(u)
+
+R = reduce(lambda m, n: m+n, X)
+
+print(R)
